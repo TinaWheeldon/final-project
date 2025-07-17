3ccc98862e9c5a1a4b16757998d4f1cdaebbc680
TinaWheeldon <tina.wheeldon@gmail.com>
2025-07-17 11:09:40 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-//http:www.omdbapi.com/?apikey=[yourkey]&
-//http:img.omdbapi.com/?apikey=[yourkey]&
+//http://img.omdbapi.com/?apikey=[yourkey]&
+//"http://www.omdbapi.com/?apikey=[9aa666f1]&"
+
+
+console.log(fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=9aa666f1"))
