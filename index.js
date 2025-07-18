fc55f2199bf6ec3071a3f289f06d5349e7567b80
TinaWheeldon <tina.wheeldon@gmail.com>
2025-07-18 12:00:03 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 //http://img.omdbapi.com/?apikey=9aa666f1&
 //http://www.omdbapi.com/?apikey=9aa666f1&
 //
-const titleRef = document.querySelector(".title")
 
-async function main() {
-    const response = await fetch ("http://www.omdbapi.com/?i=tt3896198&apikey=9aa666f1")
+async function fetchData() {
+    const movieTitle = document.getElementById("movieTitle")
+    const response = await fetch ("http://www.omdbapi.com/?s=fast&apikey=9aa666f1")
     const data = await response.json()
     console.log(data)
-    //titleRef.innerHTML = data.title
 }
 
-main ()
+fetchData ()
 
 const backgroundImg = document.querySelector(".background__img")
 backgroundImg.scr = "./final assets/theater curtains.html"
