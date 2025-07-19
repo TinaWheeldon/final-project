ea6460381be0058b54513bd77cc3f985b4815ef9
TinaWheeldon <tina.wheeldon@gmail.com>
2025-07-19 11:09:05 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ fetchData()
 
 //const backgroundImg = document.querySelector(".background__img")
 //backgroundImg.scr = "./final assets/theater curtains.html"
+
+const movieListEl = document.querySelector(".movie-list")
+
+async function main() {
+const movie = await fetch("http://www.omdbapi.com/?s=fast&apikey=9aa666f1")
+const movieData = await movie.json()
+movieListEl.innerHTML = movieData.map((movie) => movieHTML(movie)).join("")
+}
+
+main()
+
+function movieHTML(movie) {
+        return `<div class="movie">
+            <div class="movie__container">
+                <div class="movie-list">
+                 <h1>Movie</h1>
+                <p>title</p>
+                <p>year</p>
+                <p>poster</p>    
+                </div>
+            </div>
+        </div>`
+}
